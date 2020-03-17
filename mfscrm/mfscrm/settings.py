49c7fdbcee3317410ba021058fa48c8881a14f6b
pubali25 <59894675+pubali25@users.noreply.github.com>
2020-03-17 15:05:57 -0500

    debug is false

--- mfscrm/mfscrm/settings.py
+++ mfscrm/mfscrm/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '(yw6(_vyz_!^4ph==@03e28h06)%75_45alkz(_d-j_&)p&ojv'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
